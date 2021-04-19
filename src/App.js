aa12032010e00100a98cfb01b9c95fbd49e9d381
Chase Ainsworth <chase.ainsworth@codeimmersives.com>
2021-04-19 04:20:08 -0400

    input working, list populating

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { GroceryInputContext, GroceryContext } from "./context/context";
+import { v4 as uuidv4 } from "uuid";
 import './App.css';
 
+import GroceryInput from "./GroceryInput";
+import Grocery from "./Grocery"
+
+let groceryObj  = [
+  {
+    id: uuidv4(),
+    grocery: "bread",
+  },
+  {
+    id: uuidv4(),
+    grocery: "milk",
+  }
+]
+
 function App() {
+  const [groceryArray, setGroceryArray] = useState(groceryObj)
+
+  function addGrocery(grocery) {
+    let newAddedGroceryArray = [
+      ...groceryArray,
+      {
+        id: uuidv4(),
+        grocery,
+      },
+    ]
+    setGroceryArray(newAddedGroceryArray);
+  }
+
+  function showGroceryInput() {
+    return (
+      <GroceryInputContext.Provider value={{addGrocery}}>
+        <GroceryInput/>
+      </GroceryInputContext.Provider>
+    )
+  }
+
+  function showGrocery() {
+    return groceryArray.map((item) => {
+      return (
+        <GroceryContext.Provider key={item.id} value={{groceryItem: item}}>
+          <Grocery />
+        </GroceryContext.Provider>
+      )
+    })
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {showGroceryInput()}
+      {showGrocery()}
     </div>
   );
 }
---
+++ src/Grocery.js
@@ -0,0 +1,17 @@
+import React, { useContext } from 'react'
+import { GroceryContext } from "./context/context"
+
+function Grocery() {
+    
+    const {
+        groceryItem: {id, grocery}
+    } = useContext(GroceryContext)
+
+    return (
+        <div>
+            {grocery}
+        </div>
+    )
+}
+
+export default Grocery
